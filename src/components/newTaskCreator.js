067bd914577c7bbc1037afb5484341ce89c2dcca
adnanyusuf85 <adnan.yusufs@gmail.com>
2025-06-30 11:13:20 +0500

    WIP: generalizing screen overlay

--- src/components/newTaskCreator.js
+++ src/components/newTaskCreator.js
@@ -4,11 +4,13 @@ import * as style from "./task-creator-style.module.css"
 class NewTaskCreatorUIComp{
     constructor(task){
         this.task = task;
+        this.container = document.createElement('div');
+        this.container.className = style.container;
+        this.container.id = "new-task-creator-window";
+        this.created = true;
     }
 
-    getDOM(){
-        var container = document.createElement('div');
-        container.className = style.container;
+    getDOM(){    
 
         var projectNameLabel = document.createElement('label');
         projectNameLabel.innerHTML = "Project Name";
@@ -29,6 +31,13 @@ class NewTaskCreatorUIComp{
 
         var taskDescription = document.createElement('textarea');
 
+        var dueDateLabel = document.createElement("label");
+        dueDateLabel.innerHTML = "Due Date";
+
+        var dueDateElement = document.createElement("input");
+        dueDateElement.setAttribute("type", "date");
+
+
         var tag = document.createElement('p');
         var taglist = ['Urgent', 'New', 'Technology'];
         taglist.forEach(tagItem =>{
@@ -40,20 +49,30 @@ class NewTaskCreatorUIComp{
 
         var cancelButton = document.createElement('button');
         cancelButton.innerHTML = "Cancel";
+        cancelButton.addEventListener('click', () => this.closeSubWindow());
 
-        container.append(
+        this.container.append(
             projectNameLabel,
             projectName,
             taskTitleLabel,
             taskTitle,
             taskDescriptionLabel,
             taskDescription,
+            dueDateLabel,
+            dueDateElement,
             tag,
             editButton,
             cancelButton
         );
-        return container;
+
+        
+        return this.container;
     }
+
+    closeSubWindow(){
+        this.container.remove();
+    }
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,24 @@ task1.title = "my task";
 
 let para = document.createElement("p");
 para.innerHTML = task1.title;
+
+let newTaskButton = document.createElement('button');
+newTaskButton.textContent = "Add New Task";
+newTaskButton.addEventListener("click",addNewTask);
 document.body.appendChild(para);
+document.body.appendChild(newTaskButton);
 
-let newTaskCreator = new NewTaskCreatorUIComp(new Task("My Task"));
+let overlay_screen = document.createElement('div');
+overlay_screen.id = "overlay-screen";
+overlay_screen.classList.add("focused_overlay");
+overlay_screen.classList.add("hidden");
+document.body.appendChild()
 
-document.body.appendChild(newTaskCreator.getDOM());
+
+
+function addNewTask(){
+    let newTaskCreator = new NewTaskCreatorUIComp(new Task("My Task"));
+    let newTaskCreatorElement = newTaskCreator.getDOM();
+    let overlayscreen = document.getElementById("overlay-screen");
+    overlayscreen.appendChild(newTaskCreatorElement);
+}
